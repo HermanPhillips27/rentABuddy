e10e663db5cc65e00c8867b09f91de61ecc43684
Herman Phillips <61162727+HermanPhillips27@users.noreply.github.com>
2020-08-29 14:32:10 -0400

    teachers database should work now

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -48,11 +48,9 @@ db.Buddy
   .then(() => db.Buddy.insertMany(buddiesSeed))
   .then(data => {
     console.log(data);
-    process.exit(0);
   })
   .catch(err => {
     console.error(err);
-    process.exit(1);
   });
 
   db.Teacher
